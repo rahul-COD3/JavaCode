5818f7e54b0cb676bf1ae478dbd1465b708f58c2
rahul138 <rahul.kumar17407@marwadieducation.edu.in>
2022-07-13 13:52:41 +0530

    done

---
+++ GFG_try/hello.java
@@ -0,0 +1,9 @@
+package GFG_try;
+
+public class hello {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        System.out.println(a + b);
+    }
+}
---
+++ LinkedList/linkedListCreate.java
@@ -0,0 +1,43 @@
+package LinkedList;
+
+public class linkedListCreate {
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        Node temp1 = new Node(20);
+        Node temp2 = new Node(30);
+        head.next = temp1;
+        temp1.next = temp2;
+        printList(head);
+    }
+
+    static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + "\t");
+            curr = curr.next;
+        }
+    }
+
+    static void recPrintList(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.data + "\t");
+        recPrintList(head.next);
+
+    }
+}
+
+/**
+ * Node
+ */
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
